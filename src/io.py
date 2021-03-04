af40998be9cd9a2f4d01346d9f391b980ff03ad2
Tony <20404466+e-tony@users.noreply.github.com>
2021-03-04 21:39:32 +0100

    Create io.py

---
+++ src/io.py
@@ -0,0 +1,35 @@
+from typing import List, Dict
+
+
+class Token():
+    # form: str = ""
+    # pos: str = ""
+    # head: int = 0
+
+    def __init__(self, t: Dict):
+        self.id = t['ID']
+        self.form = t['FORM']
+        self.lemma = t['LEMMA']
+        self.pos = t['POS']
+        self.xpos = t['XPOS']
+        self.morph = t['MORPH']
+        self.head = t['HEAD']
+        self.rel = t['REL']
+        self.x1 = t['X1']
+        self.x2 = t['X2']
+
+
+class Sentence():
+    # tokens: List[Token]
+
+    def __init__(self, sentence: List[Dict]):
+        self.tokens = self.convert2tokens(sentence)
+
+    def convert2tokens(self, sentence: List[Dict]):
+        tokens = []
+
+        for i, d in enumerate(sentence):
+            token = Token(d)
+            tokens.append(token)
+        
+        return tokens

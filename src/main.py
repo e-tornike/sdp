d6a6942183c7434c07750dd8808bb44fcdeb1050
Tony <20404466+e-tony@users.noreply.github.com>
2021-03-05 18:38:46 +0100

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,114 @@
+import sys
+sys.path.insert(0, '.')
+from src.io import Sentence, Token
+from src.reader import read_file
+from src.writer import write_file
+from src.metrics import evaluate
+from src.state import State
+from src.parser import ArcStandard
+from src.parser_utils import getArcs
+from src.feature_mapper import FeatureMap, FeatureItem
+from src.model import Perceptron
+from src.util import addHeads, convert_to_dict
+from pathlib import Path
+from typing import List, Tuple
+from copy import copy
+import pickle
+import time
+import numpy as np
+import random
+from sklearn.metrics import f1_score, accuracy_score
+from tqdm import tqdm
+import json
+
+
+def test(data, model, fm, parser, path, debug=False):
+    preds = []
+
+    for i, sentence in enumerate(tqdm(data)):
+        state = State(sentence)
+
+        pred_arcs, pred_seq = parser.parse(state, sentence, fm, model, debug=debug)
+
+        pred_sentence = addHeads(sentence, pred_arcs)
+        preds.append(pred_sentence)
+    
+    write_file(path.replace("blind", "preds"), preds)
+
+def main(path_train, path_dev, path_test, language, epochs=5, shuffle=True, debug=False):
+    print("Reading files...")
+    sentences_train = read_file(path_train, lang=language)
+    sentences_dev = read_file(path_dev, lang=language)
+    sentences_test = read_file(path_test, lang=language)
+
+    fm = FeatureMap()
+    parser = ArcStandard()
+    labels = {"LARC": 0, "RARC": 1, "SHIFT": 2}
+
+    print("Extracting features...")
+
+    train_data = []
+
+    for i, sentence in enumerate(tqdm(sentences_train)):
+        state = State(sentence)
+        _, gold_seq = parser.oracleParse(state, sentence, fm, debug=debug)
+        train_data += gold_seq
+
+    fm.freeze = True
+
+    print("Done.")
+
+    print("Training...")
+    
+    model = Perceptron(fm.feature_map, labels)
+    model.train(train_data, epochs=epochs, shuffle=shuffle)
+
+    print("Done.")
+
+    print("Testing...")
+
+    test(sentences_train, model, fm, parser, path_train, debug)
+    test(sentences_dev, model, fm, parser, path_dev, debug)
+    test(sentences_test, model, fm, parser, path_test, debug)
+
+    print("Saving model...")
+
+    model.save(f"lang_{language}_epochs_{str(epochs)}")
+
+    print("Done.")
+
+def eval(path_gold):
+    sentences_gold = read_file(path_gold.replace(".blind", ".gold"))
+    sentences_pred = read_file(path_gold.replace(".blind", ".preds"))
+    
+    gold_dict = convert_to_dict(sentences_gold)
+    pred_dict = convert_to_dict(sentences_pred)
+
+    return evaluate(gold_dict, pred_dict)
+
+
+if __name__ == "__main__":
+    # path_train = "/home/tony/Coding/MA/ws2021/sdp/data/english/train/wsj_train.only-projective.conll06"
+    # path_dev = "/home/tony/Coding/MA/ws2021/sdp/data/english/dev/wsj_dev.conll06.blind"
+    # path_test = "/home/tony/Coding/MA/ws2021/sdp/data/english/test/wsj_test.conll06.blind"
+    # lang = "en"
+    # debug = False
+
+    # main(path_train, path_dev, path_test, lang, epochs=20, debug=debug)
+
+    # print("Train acc:", eval(path_train))
+    # print("Dev acc:", eval(path_dev))
+
+    print("================")
+
+    path_train = "/home/tony/Coding/MA/ws2021/sdp/data/german/train/tiger-2.2.train.only-projective.conll06"
+    # path_train = "/home/tony/Coding/MA/ws2021/sdp/data/german/train/example.train.conll06"
+    path_dev = "/home/tony/Coding/MA/ws2021/sdp/data/german/dev/tiger-2.2.dev.conll06.blind"
+    path_test = "/home/tony/Coding/MA/ws2021/sdp/data/german/test/tiger-2.2.test.conll06.blind"
+    lang = "en"
+    debug = False
+
+    main(path_train, path_dev, path_test, lang, epochs=5, debug=debug)
+
+    print("Train acc:", eval(path_train))
+    print("Dev acc:", eval(path_dev))
